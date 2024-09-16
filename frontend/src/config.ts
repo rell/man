e4d70a8bc2ed20d7521054c6adcefe65dce4f130
Rell Credle <ter@rel.lc>
2024-09-16 06:57:13 -0400

    Update config.ts

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,8 @@
 /**
  * All the configuration variables are stored here
+ top is local 
+ second is prod
  */
-const API_BASE_URL = "http://localhost:8000";
+/* const API_BASE_URL = "http://localhost:8000"; */
+const API_BASE_URL = "http://<ec2-ip>/api"
 export default API_BASE_URL;
