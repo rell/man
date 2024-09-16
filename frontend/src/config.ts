799ddf959229cd7dd73bc21f933c683a09989a67
Rell Credle <ter@rel.lc>
2024-09-16 14:33:29 -0400

    Update config.ts

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -4,5 +4,5 @@
  second is prod
  */
 /* const API_BASE_URL = "http://localhost:8000"; */
-const API_BASE_URL =  `http://${process.env.AWS_PUB_DNS}/api`;
+const API_BASE_URL =  `${process.env.AWS_PUB_DNS}/api`;
 export default API_BASE_URL;
