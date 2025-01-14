ba8c0267335a0e0d74dc0cf3def7942a3eca250c
Terrell Credle <ter@rel.lc>
2025-01-14 12:29:25 -0500

    Update index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "leaflet/dist/leaflet.css";
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client"; 
 import App from "./App";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root"),
+  </React.StrictMode>
 );
