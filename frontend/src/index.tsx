1fe5956037f47234c7eba36d62f096f9d8de9fe7
Terrell Credle <19939107+rell@users.noreply.github.com>
2025-01-27 14:57:38 -0500

    init

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,21 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "leaflet/dist/leaflet.css";
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom/client"; 
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-=======
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App />);
->>>>>>> ee75fc9 (init)
